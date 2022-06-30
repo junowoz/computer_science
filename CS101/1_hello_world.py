2ac7b5c46e4e38ff40e5c9b839860651e4f1c7da
junowoz <junogouvea@gmail.com>
2022-06-30 18:06:40 -0400

    python 30 jun

---
+++ CS101/1_hello_world.py
@@ -0,0 +1,46 @@
+#WELCOME TO CS101
+
+print("Hello and welcome " + my_name + "!")
+print('Hello')
+#print('This message has mismatched quote marks!") #ERROR
+
+#Comment
+
+variable = 30
+my_name = "Juan"
+
+print(25 * 68 + 13 / 28)
+
+#Exponents 
+print(5 ** 2) # 5²
+
+#Modulo
+# Prints 4 because 29 / 5 is 5 with a remainder of 4
+print(29 % 5)
+ 
+# Prints 2 because 32 / 3 is 10 with a remainder of 2
+print(32 % 3)
+
+#Turn a Number to a String
+number = 14
+print(str(number))
+
+#Concatenation
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity."
+message = string1 + string2
+
+print(message)
+
+#Plus Equals
+number = 2
+number += 2
+print(number) #Prints 4
+
+#Multi-line Strings: (""" or ''')
+
+to_you = '''Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?'''
+
+print(to_you)
