e6b2e51ec36ab515e9cca3e70459e5870374d998
junowoz <junogouvea@gmail.com>
2023-02-15 22:46:56 -0400

    control flow

---
+++ CS101/2_control_flow.py
@@ -0,0 +1,17 @@
+# CONTROL FLOW
+# In Python, your script will execute from the top down, until there is nothing left to run.
+
+#Booleans
+boolean = True
+booleanFalse = False
+
+#Relational Operators
+#Equals:
+10 == 10  #True
+'10' == 10 #False
+
+#Not equals:
+10 != 10 #False
+
+#Boolean Variables
+
